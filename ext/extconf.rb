3b5bf2cf3a8f9b00cd2fcaaeafc1df905a1e2331
Andrew Ordiales <pair+andrew@friendster.com>
2013-02-08 17:29:10 +0800

    Added: Ext Hack

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,3 +2,4 @@ require File.join File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'chrome
 require File.join File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'chrome_plating', 'mac_helper'
 
 ChromePlating::MacHelper.new.install_chromedriver if ChromePlating.platform == 'darwin'
+ChromePlating::MacHelper.new.send_ok
